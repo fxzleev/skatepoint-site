80dcb6b2bc2a553d31072dcea82cfa923757e510
fxzleev <f4zleev@gmail.com>
2024-09-28 01:03:00 +0300

    fix background path 4

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
+	resolve: {
+		alias: {
+			$image: resolve('./src/assets/img')
+		}
+	},
+	
 	css: {
   	preprocessorOptions: {
     	scss: {
