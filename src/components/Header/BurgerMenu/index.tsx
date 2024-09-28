6b24936aebc9f5ffe82ca8f9b2ae75a059f52b14
fxzleev <f4zleev@gmail.com>
2024-09-29 00:15:53 +0300

    fix burger icon 1

--- src/components/Header/BurgerMenu/index.tsx
+++ src/components/Header/BurgerMenu/index.tsx
@@ -1,4 +1,5 @@
 import { FC, Dispatch } from 'react'
+import menuIcon from '../../../assets/img/menu.png'
 
 type Props = {
 	isOpen: boolean
@@ -10,10 +11,10 @@ export const BurgerMenu: FC<Props> = ({ isOpen, setIsOpen }) => {
 	}
 
 	return (
-		<div className="burger" onClick={handleClick}>
+		<div className='burger' onClick={handleClick}>
 			<input type='checkbox' checked={isOpen} id='' />
-			<div className="burger__hamburger_lines">
-				<img src="./src/assets/img/menu.png" alt="menu" />
+			<div className='burger__hamburger_lines'>
+				<img src={menuIcon} alt='menu' />
 			</div>
 		</div>
 	)
